512c1c2249451291c9a86b657649fe2ebbaeff54
dd-mahn <ducmanh0801@gmail.com>
2024-03-12 01:25:49 +0700

    Refactor encryption and decryption logic, add input validation

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from decrypt import decrypt
 from encrypt import encrypt
 from generate_keypair import generate_keypair
-
+from input import paramInput
 
 if __name__ == '__main__':
-    print("RSA Encrypter/ Decrypter")
-    p = int(input("Enter a prime number (17, 19, 23, etc): "))
-    q = int(input("Enter another prime number (Not one you entered above): "))
-    print("Generating your public/private keypairs now . . .")
+    p, q = paramInput()
     public, private = generate_keypair(p, q)
     print("Your public key is ", public ," and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
+    number = input("Enter a number to encrypt with your public key: ")
+    print(number)
+    encrypted_msg = encrypt(public, number)
     print("Your encrypted message is: ")
     print(''.join(map(lambda x: str(x), encrypted_msg)))
-    print("Decrypting message with public key ", public ," . . .")
+    print("Decrypting message with private key ", private ," . . .")
     print("Your message is:")
-    print(decrypt(public, encrypted_msg))
+    print(decrypt(private, encrypted_msg))
