a6e93eff789aa5e5d9f8fbeac5ec782c4fb1c366
dd-mahn <ducmanh0801@gmail.com>
2024-03-11 17:10:56 +0700

    Add RSA encryption and decryption functionality

---
+++ main.py
@@ -0,0 +1,19 @@
+from decrypt import decrypt
+from encrypt import encrypt
+from generate_keypair import generate_keypair
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public ," and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public ," . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
