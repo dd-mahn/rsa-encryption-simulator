f20bd3eca6f1d0862912f8777752a9ec188ae12c
dd-mahn <ducmanh0801@gmail.com>
2024-03-12 03:12:29 +0700

    Refactor code and remove unnecessary comments and unused imports

--- main.py
+++ main.py
@@ -2,22 +2,8 @@ from utils.is_prime import is_prime
 from utils.decrypt import decrypt
 from utils.encrypt import encrypt
 from utils.generate_keypair import generate_keypair
-from utils.input import paramInput
 from utils.make_prime import make_prime
 from flask import Flask, request, render_template
-
-# if __name__ == '__main__':
-#     p, q = paramInput()
-#     public, private = generate_keypair(p, q)
-#     print("Your public key is ", public ," and your private key is ", private)
-#     number = input("Enter a number to encrypt with your public key: ")
-#     print(number)
-#     encrypted_msg = encrypt(public, number)
-#     print("Your encrypted message is: ")
-#     print(''.join(map(lambda x: str(x), encrypted_msg)))
-#     print("Decrypting message with private key ", private ," . . .")
-#     print("Your message is:")
-#     print(decrypt(private, encrypted_msg))
 
 app = Flask(__name__)
 app.template_folder = 'templates'
@@ -26,6 +12,7 @@ app.static_folder = 'static'
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        all_prime = True
         p = int(request.form.get('p'))
         q = int(request.form.get('q'))
         message = request.form.get('message')
@@ -43,8 +30,24 @@ def home():
         
         decrypted_msg = decrypt(private, encrypted_msg)
         
-        return render_template('index.html',all_prime=all_prime, public_key=public, private_key=private, encrypted_msg=encrypted_msg, decrypted_msg=decrypted_msg, p = p, q = q, message = message)
+        return render_template('index.html',all_prime=all_prime, public_key=public, private_key=private, encrypted_msg=encrypted_msg, decrypted_msg=decrypted_msg, primeP=p, primeQ=q, message = message)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+    
+    
+# Terminal version:
+# from utils.input import paramInput
+# if __name__ == '__main__':
+#     p, q = paramInput()
+#     public, private = generate_keypair(p, q)
+#     print("Your public key is ", public ," and your private key is ", private)
+#     number = input("Enter a number to encrypt with your public key: ")
+#     print(number)
+#     encrypted_msg = encrypt(public, number)
+#     print("Your encrypted message is: ")
+#     print(''.join(map(lambda x: str(x), encrypted_msg)))
+#     print("Decrypting message with private key ", private ," . . .")
+#     print("Your message is:")
+#     print(decrypt(private, encrypted_msg))
